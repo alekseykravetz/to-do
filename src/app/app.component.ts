02d2341e7394fd1235fa08134b811f701f6cf130
Aleksey Kravetz <aleksey.kravetz@gmail.com>
2018-04-01 14:37:14 +0300

    add Primeng + Font-Awsome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SelectItem } from 'primeng/api';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  cities: SelectItem[];
+  selectedCity: string;
+
+  @ViewChild('myB') myB;
+
+  constructor() {
+    this.cities = [];
+    this.cities.push({ label: 'New York', value: 1 });
+    this.cities.push({ label: 'Rome', value: 2 });
+    this.cities.push({ label: 'London', value: 3 });
+    this.cities.push({ label: 'Istanbul', value: 4 });
+    this.cities.push({ label: 'Paris', value: 5 });
+  }
+
+
+
+  handleClick() {
+    console.log('clicked');
+    console.log(this.selectedCity);
+  }
+
+  dropdownSelectionChanged() {
+    console.log('dropdownSelectionChanged');
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AccordionModule } from 'primeng/accordion';     // accordion and accordion tab
+import { MenuItem, SelectItem } from 'primeng/api';                 // api
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
+import { DropdownModule } from 'primeng/dropdown';
+
+// import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +20,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+
+    ButtonModule,
+    TooltipModule,
+    AccordionModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
